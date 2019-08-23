ee52d1c089423d2cd31d54120a352e25fa0345fc
zhaojianyin <zhaojianyin@guazi.com>
2019-08-23 14:32:38 +0800

    FizzBuzz

---
+++ src/main/java/leetcode/Solution412.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ *
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ *
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ *
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fizz-buzz
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author zhaojianyin
+ * @create 2019-08-23 下午2:31
+ */
+public class Solution412 {
+	public List<String> fizzBuzz(int n) {
+		List<String> list = new ArrayList();
+		for (int i = 1; i <= n; i++) {
+			if (i % 15 == 0) {
+				list.add("FizzBuzz");
+			} else if (i % 3 == 0) {
+				list.add("Fizz");
+			} else if (i % 5 == 0) {
+				list.add("Buzz");
+			} else {
+				list.add(i + "");
+			}
+		}
+		return list;
+	}
+}
