577d3cb93ce4b10149cac23b96d3b3de3d1f15b3
zhaojianyin <zhaojianyin@guazi.com>
2019-09-11 20:00:42 +0800

    单利

---
+++ src/main/java/util/SingletonTest.java
@@ -0,0 +1,15 @@
+package util;
+
+/**
+ * @author zhaojianyin
+ * @create 2019-09-11 下午7:47
+ */
+public class SingletonTest
+{
+
+	public static void main(String[] args)
+	{
+//		Singleton.getInstance();
+        Singleton.display();
+	}
+}
