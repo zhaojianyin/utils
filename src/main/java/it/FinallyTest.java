d829041cd59d65fb2bf1314fc46a7afcf790adeb
zhaojianyin <zhaojianyin@guazi.com>
2019-10-11 11:21:27 +0800

    finally

---
+++ src/main/java/it/FinallyTest.java
@@ -0,0 +1,28 @@
+package it;
+
+/**
+ * @author zhaojianyin
+ * @create 2019-10-11 上午10:18
+ */
+public class FinallyTest {
+
+	public static void main(String[] args) {
+		Thread t = new Thread(new Daemon());
+		t.start();
+	}
+}
+
+
+class Daemon implements Runnable {
+	@Override
+	public void run() {
+		try {
+			System.out.println("1");
+		} catch (Exception e) {
+			System.out.println("2");
+		} finally {
+			Thread.currentThread().stop();
+			System.out.println("3");
+		}
+	}
+}
