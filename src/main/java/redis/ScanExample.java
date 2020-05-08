4181549ad024af7cf16487c50d54489f9396311d
zhaojianyin <384144795@qq.com>
2020-05-08 15:29:22 +0800

    臊面

--- src/main/java/redis/ScanExample.java
+++ src/main/java/redis/ScanExample.java
@@ -14,7 +14,7 @@ import util.JedisUtils;
 public class ScanExample {
 	public static void main(String[] args) {
 		// 添加 10w 条数据
-//		initData();
+		initData();
 		BasicIndexableFieldIdentifier();
 	}
 
